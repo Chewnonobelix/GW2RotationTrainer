7e9c6e11d79d1a9d535eec139a3efb621cd3fb6b
Arnaud DUHAMEL <arnaud.duhamel.pro@gmail.com>
2022-09-28 17:02:18 +0200

    save db rotation

--- database.cpp
+++ database.cpp
@@ -48,5 +48,24 @@ void DataBase::addBuild(QJsonObject build)
 {
     qDebug()<<build;
     auto query = m_db.exec(QString("INSERT INTO builds (name) VALUES('%1')").arg(build["name"].toString()));
-    qDebug()<<"Insert build"<<query.lastError();
+    auto id = query.lastInsertId();
+    qDebug()<<"Insert build"<<query.lastError()<<id;
+
+    auto opening = build["opening"].toArray();
+    auto rotation = build["rotation"].toArray();
+
+    auto inserter = [id, this](QJsonArray array, bool isOpening) {
+        for(auto i = 0; i < array.count(); i++) {
+            auto queryRole = m_db.exec(QString("SELECT id FROM mapping WHERE role='%1'").arg(array[i].toObject()["role"].toString()));
+            queryRole.next();
+            auto role = queryRole.record().value("id").toInt();
+            auto queryRot = m_db.exec(QString("INSERT INTO rotation (build, role, opening, 'index') VALUES ('%1', '%2', '%3', '%4')")
+                                      .arg(id.toInt()).arg(role).arg(isOpening).arg(i));
+
+            qDebug()<<"Rot insert"<<queryRot.lastQuery()<<queryRot.lastError();
+        }
+    };
+
+    inserter(opening, true);
+    inserter(rotation, false);
 }
